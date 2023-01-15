cd47d04d464027eab76066068ee5a19227c02510
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-15 19:37:30 +0200

    Add rolling a negative number test

--- bowling_game.py
+++ bowling_game.py
@@ -1,3 +1,7 @@
 class Game():
     def score(self):
         return 0
+    
+    def roll(self, pins: int):
+        if pins < 0:
+            raise ValueError("Pins must be equal or greater than 0.")
--- bowling_game_test.py
+++ bowling_game_test.py
@@ -3,6 +3,14 @@ from unittest import TestCase
 from bowling_game import Game
 
 class TestBowlingGame(TestCase):
+    def setUp(self) -> None:
+        self.game = Game()
+
     def test_create_game(self):
-        game = Game()
-        self.assertEqual(game.score(), 0)
+        self.assertEqual(self.game.score(), 0)
+    
+    def test_roll_negative_number(self):
+        with self.assertRaises(ValueError) as e:
+            self.game.roll(-1)
+        
+        self.assertEqual(str(e.exception), "Pins must be equal or greater than 0.")
