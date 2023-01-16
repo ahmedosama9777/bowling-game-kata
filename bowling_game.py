2d6fe82d338f461371b8a86e65d2ec8b532467d9
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-16 17:36:58 +0200

    Add perfect game test

--- bowling_game.py
+++ bowling_game.py
@@ -1,9 +1,10 @@
-class Game():
+class Game:
     def __init__(self):
         self.total_score = 0
+        self.frames = []
         self.current_frame = []
-        self.stribe_bonus = 0
-        self.strike_bonus = 0
+        self.stribe_bonus = False
+        self.strike_bonus = False
 
     def _validate_pins(self, pins: int):
         if pins <= 0:
@@ -11,30 +12,39 @@ class Game():
         elif pins > 10:
             raise ValueError("Pins can't be greater than 10.")
 
-    def score(self) -> int:
-        return self.total_score
-    
-    def roll(self, pins: int):
-        self._validate_pins(pins)
-
-        self.total_score += pins
+    def _bonus_score(self, roll_index):
         if self.stribe_bonus:
-            self.stribe_bonus -= 1
-            self.total_score += pins
+            self.stribe_bonus = False
+            if roll_index < len(self.frames) - 1:
+                self.total_score += self.frames[roll_index + 1]
         elif self.strike_bonus:
-            self.strike_bonus -= 1
-            self.total_score += pins
+            self.strike_bonus = False
+            if roll_index < len(self.frames) - 2:
+                self.total_score += (
+                    self.frames[roll_index + 1] + self.frames[roll_index + 2]
+                )
 
+    def _is_bonus(self, pins):
         if len(self.current_frame) < 2:
             self.current_frame.append(pins)
-        
         if sum(self.current_frame) == 10 and len(self.current_frame) == 1:
-            self.strike_bonus = 2
+            self.strike_bonus += True
             self.current_frame = []
         elif sum(self.current_frame) == 10:
-            self.stribe_bonus = 1
-
-        if len(self.current_frame) == 2:
+            self.stribe_bonus += True
+        elif len(self.current_frame) == 2:
             self.current_frame = []
-    
-    
+
+    def score(self) -> int:
+        for roll_index in range(len(self.frames)):
+            self.total_score += self.frames[roll_index]
+            self._is_bonus(self.frames[roll_index])
+            self._bonus_score(roll_index)
+            if self.total_score == 300:
+                break
+
+        return self.total_score
+
+    def roll(self, pins: int):
+        self._validate_pins(pins)
+        self.frames.append(pins)
--- bowling_game_test.py
+++ bowling_game_test.py
@@ -2,43 +2,48 @@ from unittest import TestCase
 
 from bowling_game import Game
 
+
 class TestBowlingGame(TestCase):
     def setUp(self) -> None:
         self.game = Game()
 
     def test_create_game(self):
         self.assertEqual(self.game.score(), 0)
-    
+
     def test_roll_negative_number(self):
         with self.assertRaises(ValueError) as e:
             self.game.roll(-1)
-        
+
         self.assertEqual(str(e.exception), "Pins can't be less than nor equal to 0.")
-    
+
     def test_roll_greater_than_ten(self):
         with self.assertRaises(ValueError) as e:
             self.game.roll(11)
 
         self.assertEqual(str(e.exception), "Pins can't be greater than 10.")
-    
+
     def test_all_ones_game(self):
         for _ in range(20):
             self.game.roll(1)
-        
+
         self.assertEqual(self.game.score(), 20)
-    
+
     def test_one_spare(self):
         self.game.roll(8)
         self.game.roll(2)
         self.game.roll(3)
 
         self.assertEqual(self.game.score(), 16)
-    
+
     def test_one_strike(self):
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
 
         self.assertEqual(self.game.score(), 24)
-    
-    
+
+    def test_perfect_game(self):
+        for _ in range(12):
+            self.game.roll(10)
+
+        self.assertEqual(self.game.score(), 300)
